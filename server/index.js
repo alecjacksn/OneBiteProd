08aec8877b75d0f24d0b8aefd285e22a24703660
alecjacksn <alecajackson@gmail.com>
2018-05-14 05:39:15 -0600

    removed hashrouter

--- server/index.js
+++ server/index.js
@@ -28,7 +28,10 @@ app.use(express.static(`${__dirname}/../build`));
 app.use(bodyParser.json());
 app.use(cors());
 
-
+const path = require('path')
+app.get('*', (req, res)=>{
+  res.sendFile(path.join(__dirname, '../build/index.html'));
+})
 // configureServer(app);
 // configureRoutes(app);
 
--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import { HashRouter } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import store from './store'
 import HttpsRedirect from 'react-https-redirect';
@@ -12,9 +12,9 @@ import HttpsRedirect from 'react-https-redirect';
 ReactDOM.render(
     <Provider store={store} >
         <HttpsRedirect>
-            <HashRouter>
+            <BrowserRouter>
                 <App />
-            </HashRouter>
+            </BrowserRouter>
         </HttpsRedirect>
     </Provider>
     , document.getElementById('root'));
